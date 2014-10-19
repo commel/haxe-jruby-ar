47bc0d966a8591fd907b60509567b294feece13a
Bernd Ritter <commel@gmail.com>
2014-10-19 20:35:21 +0200

    Maven :-)

--- jruby-ar/buildnrun.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p classes
-
-jrubyc -t classes -d ruby --javac ruby/RubyApp.rb
-javac -d classes -cp classes:/opt/bin/jruby/lib/jruby.jar java/JavaApp.java
-java -cp classes:/opt/bin/jruby/lib/jruby.jar JavaApp
