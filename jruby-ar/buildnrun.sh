d1aca564b2ed3457fbc5d65d6b9b210319fd6069
Bernd Ritter <commel@gmail.com>
2014-10-19 10:04:39 +0200

    Better structure. Next step should be maven.

--- jruby-ar/buildnrun.sh
+++ jruby-ar/buildnrun.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-rm Sayer.*
-rm *.class
-
 mkdir -p classes
 
-jrubyc -t classes --javac RubyApp.rb
-javac -d classes -cp classes:/opt/bin/jruby/lib/jruby.jar JavaApp.java
+jrubyc -t classes -d ruby --javac ruby/RubyApp.rb
+javac -d classes -cp classes:/opt/bin/jruby/lib/jruby.jar java/JavaApp.java
 java -cp classes:/opt/bin/jruby/lib/jruby.jar JavaApp
